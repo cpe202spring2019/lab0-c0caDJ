c20319e1a8efbfb341f67c674a18506b7b03e81c
c0caDJ <nguyen.david94579@gmail.com>
2019-04-05 10:59:15 -0700

    first commit, can't pass first test

--- planets.py
+++ planets.py
@@ -1,5 +1,11 @@
 def weight_on_planets():
    # write your code here
+   normalWeight = int(input("What do you weigh on earth? "))
+   marsWeight = normalWeight * 0.38
+   jupiterWeight = normalWeight * 2.34
+
+   #print(" \nOn Mars you would weigh {} pounds. \nOn Jupiter you would weigh {} pounds." .format(marsWeight, jupiterWeight))
+   print("\nOn Mars you would weigh " +str(marsWeight)+ " pounds. \nOn Jupiter you would weigh "+str(jupiterWeight)+ " pounds." )
    
    
    
